2fe5f0d2733d0e64b1d195852a536e5f46992733
wksiriwardana <wksiriwardana@virtusa.com>
2018-01-23 19:13:32 +0530

    Uploaded

---
+++ OtcTestTemplates.java
@@ -0,0 +1,12 @@
+package common;
+
+import com.millenniumit.fortress.core.annotations.Using;
+import com.millenniumit.fortress.exchange.templates.ExchangeTemplateArchive;
+import com.millenniumit.fortress.otc.templates.OtcTemplateProvider;
+
+/**
+ * Created by hiranp on 7/23/2014.
+ */
+@Using(ExchangeTemplateArchive.class)
+public class OtcTestTemplates extends ExchangeTestTemplates {
+}
